659cf8dc9a8c3857d45f9cd65caa11c4fc755f86
psuuc-jbgarcia <gjeric54321@gmail.com>
2025-11-17 22:16:31 +0800

    sas

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+</head>
+<body>
 <?php
 session_start();
 require "User.php";
@@ -14,15 +23,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log In</title>
-</head>
-<body>
-
 <form method="POST">
     <h2>Login</h2>
     <label>Username:</label>
